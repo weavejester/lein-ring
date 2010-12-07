20c5250debc8509de4e0df86da2ea21a2ed281a1
James Reeves <jreeves@weavejester.com>
2010-12-07 23:59:31 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject lein-ring "0.1.0"
+  :description "Leiningen Ring plugin"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [ring-jetty-adapter "0.3.5"]])
---
+++ test/leiningen/test/ring.clj
@@ -0,0 +1,6 @@
+(ns leiningen.test.ring
+  (:use leiningen.ring :reload)
+  (:use clojure.test))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
