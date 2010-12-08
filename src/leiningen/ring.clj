e0e551ad3cf97a9ab8c03827987d1e250ea92f25
James Reeves <jreeves@weavejester.com>
2010-12-08 01:55:41 +0000

    Placeholder for "lein ring" task and subtasks

--- src/leiningen/ring.clj
+++ src/leiningen/ring.clj
@@ -1,1 +1,15 @@
-(ns leiningen.ring)
+(ns leiningen.ring
+  (:require [leiningen.help :as help]))
+
+(defn run [])
+
+(defn war [])
+
+(defn ring 
+  "Manage a Ring-based application."
+  {:help-arglists '([run war])
+   :subtasks [#'run #'war]}
+  ([project]
+     (println (help/help-for "ring")))
+  ([project subtask]
+     (println "Do something")))
