0168fe122963b91befe44bdd6b5f3a1c1300d226
James Reeves <jreeves@weavejester.com>
2010-12-08 01:44:08 +0000

    Fixed project.clj dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-ring "0.1.0"
   :description "Leiningen Ring plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [ring-jetty-adapter "0.3.5"]])
+                 [ring/ring-jetty-adapter "0.3.5"]
+                 [ring/ring-devel "0.3.5"]])
