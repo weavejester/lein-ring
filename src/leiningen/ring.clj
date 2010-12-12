a897d9fffead4ed1b8b89a2985c02f2d28c952dd
James Reeves <jreeves@weavejester.com>
2010-12-12 02:53:48 +0000

    Added "lein ring uberwar" command

--- src/leiningen/ring.clj
+++ src/leiningen/ring.clj
@@ -1,15 +1,17 @@
 (ns leiningen.ring
   (:use [leiningen.help :only (help-for)]
         [leiningen.ring.server :only (server)]
-        [leiningen.ring.war :only (war)]))
+        [leiningen.ring.war :only (war)]
+        [leiningen.ring.uberwar :only (uberwar)]))
 
 (defn ring 
   "Manage a Ring-based application."
-  {:help-arglists '([server war])
-   :subtasks [#'server #'war]}
+  {:help-arglists '([server war uberwar])
+   :subtasks [#'server #'war #'uberwar]}
   ([project]
      (println (help-for "ring")))
   ([project subtask & args]
      (case subtask
-       "server" (apply server project args)
-       "war"    (apply war project args))))
+       "server"  (apply server project args)
+       "war"     (apply war project args)
+       "uberwar" (apply uberwar project args))))
---
+++ src/leiningen/ring/uberwar.clj
@@ -0,0 +1,41 @@
+(ns leiningen.ring.uberwar
+  (:require [leiningen.ring.war :as war]
+            [leiningen.compile :as compile]
+            [clojure.java.io :as io]))
+
+(defn default-uberwar-name [project]
+  (or (:uberjar-name project)
+      (str (:name project) \- (:version project) "-standalone.war")))
+
+(defn jar-dependencies [project]
+  (->> (:library-path project)
+       (io/file)
+       (.listFiles)
+       (filter #(.endsWith (str %) ".jar"))))
+
+(defn jar-entries [war project]
+  (doseq [jar-file (jar-dependencies project)]
+    (let [dir-path (:library-path project)
+          war-path (war/in-war-path "/WEB-INF/lib" dir-path jar-file)]
+      (war/file-entry war project war-path jar-file))))
+
+(defn write-uberwar [project war-path]
+  (with-open [war-stream (war/create-war project war-path)]
+    (doto war-stream
+      (war/str-entry "/WEB-INF/web.xml" (war/make-web-xml project))
+      (war/dir-entry project "/WEB-INF/classes" (:compile-path project))
+      (war/dir-entry project "/WEB-INF/classes" (:source-path project))
+      (war/dir-entry project "/" (:resources-path project))
+      (jar-entries project))))
+
+(defn uberwar
+  ([project]
+     (uberwar project (default-uberwar-name project)))
+  ([project war-name]
+     (binding [compile/*silently* true]
+       (when (zero? (compile/compile project))
+         (let [war-path (war/war-file-path project war-name)]
+           (war/compile-servlet project)
+           (write-uberwar project war-path)
+           (println "Created" war-path)
+           war-path)))))
