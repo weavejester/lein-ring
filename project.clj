84ea215882c0e54c8b4362a937f024dda8f17c06
James Reeves <jreeves@weavejester.com>
2010-12-13 20:40:51 +0000

    Released version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ring "0.2.2"
+(defproject lein-ring "0.2.3"
   :description "Leiningen Ring plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
