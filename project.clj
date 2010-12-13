59f4928afcd9a21d2e6640ad8ae27e66d38c5157
James Reeves <jreeves@weavejester.com>
2010-12-13 00:59:39 +0000

    Server now automatically reloads modified source files

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.5"]
-                 [ring/ring-devel "0.3.5"]])
+                 [ring/ring-devel "0.3.5"]
+                 [ring-reload-modified "0.1.0"]])
--- src/leiningen/ring/server.clj
+++ src/leiningen/ring/server.clj
@@ -7,11 +7,16 @@
      (server project "3000"))
   ([project port]
      (let [handler-sym (get-in project [:ring :handler])
-           handler-ns  (symbol (namespace handler-sym))]
+           handler-ns  (symbol (namespace handler-sym))
+           reload-dirs [(:source-path project)]]
        (eval-in-project project
          `(do (require 'ring.adapter.jetty
                        'ring.middleware.stacktrace
+                       'ring.middleware.reload-modified
                        '~handler-ns)
               (ring.adapter.jetty/run-jetty
-                (ring.middleware.stacktrace/wrap-stacktrace ~handler-sym)
+               (-> (var ~handler-sym)
+                   (ring.middleware.stacktrace/wrap-stacktrace)
+                   (ring.middleware.reload-modified/wrap-reload-modified
+                     ~reload-dirs))
                 {:port ~(Integer/parseInt port)}))))))
