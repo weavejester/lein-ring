537fb93a81668bc33d19dd6285029086b6b6b84d
James Reeves <jreeves@weavejester.com>
2011-01-25 21:27:48 +0000

    Browser opens to 127.0.0.1 if host is 0.0.0.0

--- src/leiningen/ring/run_server.clj
+++ src/leiningen/ring/run_server.clj
@@ -19,12 +19,13 @@
     (try-ports #(jetty-server handler %)
                suitable-ports)))
 
-(defn run-server [handler port launch-browser]
+(defn run-server [handler port launch-browser?]
   (let [server    (jetty-server handler port)
         connector (first (.getConnectors server))
         host      (or (.getHost connector) "0.0.0.0")
-        port      (.getPort connector)]
+        port      (.getPort connector)
+        url-host  (if (= host "0.0.0.0") "127.0.0.1" host)]
     (println "Started server on port" port)
-    (when launch-browser
-      (browse-url (str "http://" host ":" port)))
+    (when launch-browser?
+      (browse-url (str "http://" url-host ":" port)))
     (.join server)))
