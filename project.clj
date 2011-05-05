7636fa1e212c59c58fe000d1febb96ab5f7b7fd5
Tobias Raeder <tobias.raeder@freiheit.com>
2011-05-05 13:12:52 +0200

    Using a new version of ring

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-ring "0.3.3"
+(defproject lein-ring "0.3.4"
   :description "Leiningen Ring plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-jetty-adapter "0.3.5"]
-                 [ring/ring-devel "0.3.5"]
+                 [ring/ring-jetty-adapter "0.3.8"]
+                 [ring/ring-devel "0.3.8"]
                  [ring-reload-modified "0.1.0"]])
