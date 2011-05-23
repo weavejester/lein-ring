edfc9a9d8f72570937955cb7adb6232cedac13fc
James Reeves <jreeves@weavejester.com>
2011-05-23 21:59:08 +0100

    Updated Ring dependencies to 0.3.8

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Leiningen Ring plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-jetty-adapter "0.3.7"]
-                 [ring/ring-devel "0.3.7"]
+                 [ring/ring-jetty-adapter "0.3.8"]
+                 [ring/ring-devel "0.3.8"]
                  [ring-reload-modified "0.1.0"]])
