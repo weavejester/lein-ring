617dad3a9c6b0461a119b33895acf8bb0cbe3aba
James Reeves <jreeves@weavejester.com>
2011-06-02 01:14:03 +0100

    Fixed exception occurring for dev server when :init or :destroy not set

--- src/leiningen/ring/server.clj
+++ src/leiningen/ring/server.clj
@@ -2,23 +2,27 @@
   (:use [leiningen.compile :only (eval-in-project)]))
 
 (defn- sym-and-ns [project key]
-  (let [sym (get-in project [:ring key])]
+  (if-let [sym (get-in project [:ring key])]
     [sym (symbol (namespace sym))]))
+
+(defn- require-sym [sym]
+  (if sym
+    (list `(require '~sym))))
 
 (defn server-task
   "Shared logic for server and server-headless tasks."
   [project port launch-browser]
   (let [reload-dirs [(:source-path project)]
         [handler-sym handler-ns] (sym-and-ns project :handler)
-        [init-sym init-ns] (sym-and-ns project :init)
+        [init-sym init-ns]       (sym-and-ns project :init)
         [destroy-sym destroy-ns] (sym-and-ns project :destroy)]
        (eval-in-project project
          `(do (require 'leiningen.ring.run-server
                        'ring.middleware.stacktrace
                        'ring.middleware.reload-modified
-                       '~handler-ns
-                       '~init-ns
-                       '~destroy-ns)
+                       '~handler-ns)
+              ~@(require-sym init-ns)
+              ~@(require-sym destroy-ns)
               (leiningen.ring.run-server/run-server
                 (-> (var ~handler-sym)
                     (ring.middleware.stacktrace/wrap-stacktrace)
