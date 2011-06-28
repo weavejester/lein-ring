c6228aadcefbd3ee47ff2347fd3bbe77360f93b3
James Reeves <jreeves@weavejester.com>
2011-06-28 23:27:48 +0100

    Updated ring-reload-modified to 0.1.1

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.10"]
                  [ring/ring-devel "0.3.10"]
-                 [ring-reload-modified "0.1.0"]])
+                 [ring-reload-modified "0.1.1"]])
