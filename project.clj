0cddc1da6c3817f07fa65828fd6ec154f9dca38e
James Reeves <jreeves@weavejester.com>
2011-12-16 21:59:33 +0000

    Added eval-in-leiningen to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/weavejester/lein-ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring/ring-jetty-adapter "1.0.0"]
-                 [ring/ring-devel "1.0.0"]])
+                 [ring/ring-devel "1.0.0"]]
+  :eval-in-leiningen true)
