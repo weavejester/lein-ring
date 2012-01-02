55ef7a6f53221685aa9342b5f95c043560bfdb2a
James Reeves <jreeves@weavejester.com>
2012-01-02 22:04:23 +0000

    Updated Ring dependencies to 1.0.1

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ring/ring-jetty-adapter "1.0.0"]
-                 [ring/ring-devel "1.0.0"]]
+                 [ring/ring-jetty-adapter "1.0.1"]
+                 [ring/ring-devel "1.0.1"]]
   :eval-in-leiningen true)
