edf4f111d85db1503c20fbd1c12f86759f3370ff
Levi Campbell <levicc00123@gmail.com>
2012-02-13 18:23:00 -0700

    Replaced clojure.contrib.prxml with weissjeffm's version from github as a temporary fix for bug #22

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/weavejester/lein-ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
-                 [ring/ring-devel "1.0.1"]]
+                 [ring/ring-devel "1.0.1"]
+                 [weissjeffm/clojure.prxml "1.3.0-SNAPSHOT"]]
   :eval-in-leiningen true)
