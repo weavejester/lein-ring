964c8e17be758f728bb7dd44fa8cf560cbb900c1
Jack Moffitt <jack@metajack.im>
2012-04-23 13:20:49 -0600

    Fix help task for leiningen 2.0.

--- src/leiningen/ring.clj
+++ src/leiningen/ring.clj
@@ -5,12 +5,18 @@
         [leiningen.ring.war :only (war)]
         [leiningen.ring.uberwar :only (uberwar)]))
 
+(defn- nary? [v n]
+  (some #{n} (map count (:arglists (meta v)))))
+
 (defn ring
   "Manage a Ring-based application."
   {:help-arglists '([server server-headless war uberwar])
    :subtasks [#'server #'server-headless #'war #'uberwar]}
   ([project]
-     (println (help-for "ring")))
+     (println (if (nary? #'help-for 2)
+                ;; lein2 help-for
+                (help-for project "ring")
+                (help-for "ring"))))
   ([project subtask & args]
      (case subtask
        "server"          (apply server project args)
