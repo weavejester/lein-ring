a8dd72b7ddf1ef8c12abf6a3a7e237f97d4e4a44
James Reeves <jreeves@weavejester.com>
2012-05-24 14:16:43 +0100

    Released 0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ring "0.7.0"
+(defproject lein-ring "0.7.1"
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
