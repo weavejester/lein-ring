22f7387451f0ea47ec9bf0b792f1397cadccb569
James Reeves <jreeves@weavejester.com>
2013-01-09 23:29:51 +0000

    Updated leinjacker dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/weavejester/lein-ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/data.xml "0.0.6"]
-                 [leinjacker "0.2.0"]]
+                 [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
