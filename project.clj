63e4a77c6f6a867792dab2dab80645e48dd9107c
James Reeves <jreeves@weavejester.com>
2013-08-30 11:21:34 +0100

    Added license to project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-ring "0.8.6"
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/data.xml "0.0.6"]
                  [leinjacker "0.4.1"]]
