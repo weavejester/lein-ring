974e5d304d3a025f294e8e2902abc86d0efff10e
James Reeves <jreeves@weavejester.com>
2013-11-03 06:22:23 +0000

    Released 0.8.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ring "0.8.7"
+(defproject lein-ring "0.8.8"
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
   :license {:name "Eclipse Public License"
