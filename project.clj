c8283d15609d2aa8f6be50d406993134859aa465
Mike Blume <michael.blume@climate.com>
2014-10-28 21:28:14 -0700

    ok, actually we need clojure 1.3 to compile

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/lein-ring"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.xml "0.0.6"]
                  [leinjacker "0.4.1"]
                  [javax.servlet/servlet-api "2.5"]]
