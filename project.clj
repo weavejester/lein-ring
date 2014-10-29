3e4d14d453770aefe7463ae43a08b8cda6e7f14f
Mike Blume <michael.blume@climate.com>
2014-10-28 19:23:48 -0700

    drop a couple unnecessary dependency changes

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "https://github.com/weavejester/lein-ring"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/data.xml "0.0.6"]
                  [leinjacker "0.4.1"]
-                 [ring/ring-servlet "1.2.2"]
                  [javax.servlet/servlet-api "2.5"]]
   :eval-in-leiningen true
   :java-source-paths ["java"]
