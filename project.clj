893497aa103f9c9098eb089e40fd42aa54e9c705
James Reeves <jreeves@weavejester.com>
2015-01-09 17:29:30 +0000

    Update project dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/weavejester/lein-ring"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/data.xml "0.0.6"]
-                 [leinjacker "0.4.1"]]
+  :dependencies [[org.clojure/data.xml "0.0.8"]
+                 [leinjacker "0.4.2"]]
   :eval-in-leiningen true)
