129bfd8612f0525a7f267c08f376c00c53edc5ea
James Reeves <jreeves@weavejester.com>
2015-01-09 17:23:50 +0000

    Remove explicit Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/weavejester/lein-ring"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/data.xml "0.0.6"]
+  :dependencies [[org.clojure/data.xml "0.0.6"]
                  [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
