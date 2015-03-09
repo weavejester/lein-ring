weavejester/lein-ring

commit 857cdc2e2857b3eb01aaea038988f059cd6aa0c1

Add generated namespace to :aot
Avoid warning, address #94

src/leiningen/ring/jar.clj
@@ -26,7 +26,11 @@
              (~(generate-resolve 'ring.server.leiningen/serve) '~options))))))
 
 (defn add-main-class [project]
-  (update-project project assoc :main (symbol (main-namespace project))))
+  (let [main (symbol (main-namespace project))]
+    (->
+      project
+      (update-project assoc :main main)
+      (update-project update-in [:aot] #(-> % (conj main) distinct)))))
 
 (defn jar
   "Create an executable $PROJECT-$VERSION.jar file."

commit 148afacd82c0c19064fb58aa82402f205e146315

if :aot list is [:all] don't add main ns to it
fix bug introduced in 857cdc2e28

src/leiningen/ring/jar.clj
@@ -25,12 +25,17 @@
            (defn ~'-main []
              (~(generate-resolve 'ring.server.leiningen/serve) '~options))))))
 
+(defn add-main-to-aot [aot-list main-ns]
+  (if (= aot-list [:all])
+    aot-list
+    (-> aot-list (conj main-ns) distinct)))
+
 (defn add-main-class [project]
   (let [main (symbol (main-namespace project))]
     (->
       project
       (update-project assoc :main main)
-      (update-project update-in [:aot] #(-> % (conj main) distinct)))))
+      (update-project update-in [:aot] add-main-to-aot main))))
 
 (defn jar
   "Create an executable $PROJECT-$VERSION.jar file."