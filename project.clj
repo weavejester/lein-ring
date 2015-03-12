3c91f300eeefc0ee0b80a49fecdd63082935d77d
Mike Blume <michael.blume@climate.com>
2015-03-12 14:32:31 -0700

    Add scm info to project.clj
    So that clojars doesn't link the project to my fork

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-ring "0.9.4-SNAPSHOT"
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
+  :scm {:name "lein-ring"
+        :url "https://github.com/weavejester/lein-ring"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
