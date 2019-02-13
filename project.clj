ae7cd170ea63964be9cdebcebbe08917b9bd6258
Chad Stovern <chadhs@users.noreply.github.com>
2019-02-12 20:31:52 -0600

    Update project.clj
    Via @weavejester's recommendation
    
    > If someone wants to submit a PR that mimics lein-jacker's dependency update function then that's fine by me.
    But lein-ring should just need core.unify updated to 0.5.7 to get it working again.
    Codox had the same issue.

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
-                 [leinjacker "0.4.2"]]
+                 [leinjacker "0.4.2"]
+                 [org.clojure/core.unify "0.5.7"]]
   :eval-in-leiningen true)
