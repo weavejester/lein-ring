263769904915a51fe21c3f59760e10598d00851d
dimitriospiliouras <dpiliouras@elcom.com>
2021-05-10 15:44:55 +0100

    address most of the 2nd round of feedback

--- src/leiningen/ring/uberwar.clj
+++ src/leiningen/ring/uberwar.clj
@@ -32,14 +32,14 @@
     (->> (doto (Properties.) (.load in))
          (into {}))))
 
-(defn- from-pom-properties
-  "Returns a jar name which includes the groupId, artifactId, & version,
-   as read by the pom.properties file inside the jar file found at <jar-path>."
+(defn- jar-name-from-pom-properties
   [jar-path pom-properties-jar-path]
-  (let [pom-properties-full-path (str "jar:file:" jar-path "!/" pom-properties-jar-path)
-        {:strs [groupId artifactId version]} (read-jar-pom-properties pom-properties-full-path)]
+  (let [pom-properties-full-path (str "jar:file:"
+                                      jar-path "!/"
+                                      pom-properties-jar-path)
+        {:strs [groupId artifactId version]} (read-jar-pom-properties
+                                               pom-properties-full-path)]
     (when (and groupId artifactId version)
-      ;; found pom.properties - reconstruct the jar name to avoid collisions (issue #216)
       (str groupId \- artifactId \- version ".jar"))))
 
 (defn- war-path-for-jar [^File jar]
@@ -50,18 +50,13 @@
                          (filter (some-fn javax-servlet? pom-properties?)))] ;; 2 elements maximum
       ;; Servlet container will have it's own servlet-api impl
       (when-not (some javax-servlet? javax&pom)
-        ;; we've confirmed that one of the two possible files is missing,
-        ;; so if we find one (via `first`), it will be the pom.properties
         (str "WEB-INF/lib/"
              (or (some->> (first javax&pom)
-                          (from-pom-properties (.getAbsolutePath jar)))
+                          (jar-name-from-pom-properties (.getAbsolutePath jar)))
                  ;; fallback to using the original jar name (best-effort approach)
                  (.getName jar)))))))
 
-(defn- jar-entries
-  "Populates the 'WEB-INF/lib/' directory of the given <war> with this project's dependencies (jars).
-   If these contain a 'pom.properties' file, they will be copied into the war named as
-   $GROUP-$ARTIFACT-$VERSION.jar (see `war-path-for-jar`), otherwise using their original filename."
+(defn- populate-war-with-dependent-jars
   [war project]
   (doseq [jar-file (jar-dependencies project)]
     (when-let [war-path (war-path-for-jar jar-file)]
@@ -75,13 +70,5 @@
      (war/war
        project war-name
        :profiles-to-merge [:uberjar]
-       :additional-writes jar-entries)))
+       :additional-writes populate-war-with-dependent-jars)))
 
-(comment
-  ;; will only work under *NIX
-  (let [clojure-jar (-> (System/getProperty "user.home")
-                        (io/file ".m2/repository/org/clojure/clojure/1.10.1/clojure-1.10.1.jar"))]
-    (= "WEB-INF/lib/org.clojure-clojure-1.10.1.jar" ;; <group-id>-<artifact-id>-<version>.jar
-       (war-path-for-jar clojure-jar))
-    )
-  )
