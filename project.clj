a87e8dcbbb477f80576a2420f54884c07fd42013
Anton Mostovoy <anton@mostovoy.net>
2021-08-18 23:53:44 -0500

    Update leinjacker dependency (fixes #201)

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.unify "0.5.7"]
                  [org.clojure/data.xml "0.0.8"]
-                 [leinjacker "0.4.2"
+                 [leinjacker "0.4.3"
                   :exclusions [org.clojure/core.unify]]]
   :eval-in-leiningen true)
